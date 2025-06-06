a9110a403a7caa477488749f495d4f7678f4f6e9
Will-D7 <williamgonza777@gmail.com>
2025-06-06 15:21:52 -0400

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import RobotGame from "./pages/robotGame";
+import EditorPage from "./pages/Editors";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/robotGame" element={<RobotGame />} />
-      
+      <Route path="/"               element={<Home />} />
+      <Route path="/robotGame"      element={<RobotGame />} />
+      <Route path="/Editors"        element={<EditorPage />}/>
     </Routes>
   );
 }
